33fac84c7c34164f4fcf2643de019c8b35b72fa5
kalema1 <kalemar8@gmail.com>
2025-07-22 19:05:04 +0300

    update the uer component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -12,10 +10,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = DUMMY_USERS[0];
 
   // getter method used to get image path. used as aproperty in html
   get imagePath() {
     return `assets/users/${this.selectedUser.avatar}`;
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
