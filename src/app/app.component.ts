29f5fa383977773532b145691a8bc9e7600e2a31
kalema1 <kalemar8@gmail.com>
2025-07-24 08:13:28 +0300

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { CommonModule } from '@angular/common';
 import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, CommonModule],
+  imports: [HeaderComponent, UserComponent, CommonModule, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
