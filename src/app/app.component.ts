41eefe3c306f5dc4c9395b6172646b59f6972533
kalema1 <kalemar8@gmail.com>
2025-07-24 07:03:01 +0300

    update the app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { CommonModule } from '@angular/common';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  onSelectUser(userId: string) {
+    console.log('Selected User ID:', userId);
+  }
+}
