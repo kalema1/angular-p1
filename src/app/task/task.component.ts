1fcb5101ea43740c8f7aa972dd9099ce94b824a4
kalema1 <kalemar8@gmail.com>
2025-07-28 21:25:31 +0300

    add type to the emitter

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -18,9 +18,9 @@ type Task = {
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
 
-  @Output() complete = new EventEmitter();
+  @Output() complete = new EventEmitter<string>();
 
   onCompleteTask() {
-    this;
+    this.complete.emit(this.task.id);
   }
 }
