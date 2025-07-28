f6e1c9be50a2e00c309fb4818f651e68401a1eb9
kalema1 <kalemar8@gmail.com>
2025-07-28 21:23:49 +0300

    update the task component logic.

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 type Task = {
   id: string;
@@ -17,4 +17,10 @@ type Task = {
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
+
+  @Output() complete = new EventEmitter();
+
+  onCompleteTask() {
+    this;
+  }
 }
