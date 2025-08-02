5315abe686278bbf0bcda60e216e168e714481f9
kalema1 <kalemar8@gmail.com>
2025-08-02 17:28:57 +0300

    update the tasks component
     - added logic to display the new task component.
     - added the new task component in the the tasks component.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from '../task/task.component';
+import { NewTaskComponent } from '../new-task/new-task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) name!: string;
   @Input({ required: true }) userId!: string;
+
+  isAddingTask = false;
 
   tasks = [
     {
@@ -44,5 +47,9 @@ export class TasksComponent {
 
   onCompleteTask(taskId: string) {
     this.tasks.filter((task) => task.id !== taskId);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
